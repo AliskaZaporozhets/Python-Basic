d459995b46269d8d285639fe6455b7fa0d95a96e
AliskaZaporozhets <aliska.zaporozhets@gmail.com>
2026-01-19 22:31:19 +0200

    first commit

---
+++ classwork/1.py
@@ -0,0 +1,23 @@
+#print ('Hi PyCharm')
+
+#from math import e, pi
+import math
+
+a=10
+sum_1 = a + math.e
+sum_2 = a + math.pi
+
+print("Sum 1:", sum_1)
+print("Sum 2:", sum_2)
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ classwork/2.py
@@ -0,0 +1,4 @@
+from langdetect import detect
+
+text_1 = ('שלום אליסה')
+print(detect (text_1))
